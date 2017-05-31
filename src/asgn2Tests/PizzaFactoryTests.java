3dba41d88b503a5bae65c013c215b1e2c1e4dcdc
Daniel Gilchrist <danielgilchrist@outlook.com.au>
2017-05-31 22:17:01 +1000

    PizzaTests and PizzaFactoryTests completed (I hope)

--- src/asgn2Tests/PizzaFactoryTests.java
+++ src/asgn2Tests/PizzaFactoryTests.java
@@ -1,5 +1,16 @@
 package asgn2Tests;
 
+import java.time.LocalTime;
+import java.time.temporal.TemporalUnit;
+
+import org.junit.Test;
+
+import asgn2Exceptions.PizzaException;
+import asgn2Pizzas.MargheritaPizza;
+import asgn2Pizzas.MeatLoversPizza;
+import asgn2Pizzas.Pizza;
+import asgn2Pizzas.PizzaFactory;
+import asgn2Pizzas.VegetarianPizza;
 
 /** 
  * A class that tests the asgn2Pizzas.PizzaFactory class.
@@ -8,5 +19,102 @@ package asgn2Tests;
  * 
  */
 public class PizzaFactoryTests {
-	// TO DO
+	private static final int QUANTITY = 3;
+	private static final LocalTime ORDER_TIME = LocalTime.of(20, 0);
+	private static final LocalTime DELIVERY_TIME = LocalTime.of(20, 15);
+	private static final double MEATLOVERS_PRICE = 12.0;
+	private static final double VEGETARIAN_PRICE = 10.0;
+	private static final double MARGHERITA_PRICE = 8.0;
+	private static String[] pizzaCodes = {"PZL", "PZV", "PZM"};
+	
+	// tests PizzaFactory for all pizzas with valid parameters
+	@SuppressWarnings("unused")
+	@Test
+	public void testPizzaFactoryLegalValues() throws PizzaException {
+		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, ORDER_TIME, DELIVERY_TIME);
+		Pizza vegetarian = PizzaFactory.getPizza(pizzaCodes[1], QUANTITY, ORDER_TIME, DELIVERY_TIME);
+		Pizza margherita = PizzaFactory.getPizza(pizzaCodes[2], QUANTITY, ORDER_TIME, DELIVERY_TIME);
+	}
+	
+	@SuppressWarnings("unused")
+	@Test (expected = PizzaException.class)
+	public void testPizzaFactoryInvalidPizzaCode() throws PizzaException {
+		Pizza meatlovers = PizzaFactory.getPizza("", QUANTITY, ORDER_TIME, DELIVERY_TIME);
+	}
+	
+	@SuppressWarnings("unused")
+	@Test (expected = PizzaException.class)
+	public void testPizzaFactoryQuantityTooLow() throws PizzaException {
+		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], 0, ORDER_TIME, DELIVERY_TIME);
+	}
+	
+	@SuppressWarnings("unused")
+	@Test (expected = PizzaException.class)
+	public void testPizzaFactoryQuantityTooHigh() throws PizzaException {
+		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], 11, ORDER_TIME, DELIVERY_TIME);
+	}
+	
+	@SuppressWarnings("unused")
+	@Test (expected = PizzaException.class)
+	public void testPizzaFactoryOrderTooEarly() throws PizzaException {
+		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, LocalTime.of(6, 0), DELIVERY_TIME);
+	}
+	
+	@SuppressWarnings("unused")
+	@Test
+	public void testPizzaFactoryOrderOnOpeningTime() throws PizzaException {
+		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, LocalTime.of(7, 0), DELIVERY_TIME);
+	}
+	
+	@SuppressWarnings("unused")
+	@Test
+	public void testPizzaFactoryOrderOnCloseTime() throws PizzaException {
+		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, LocalTime.of(11, 0), DELIVERY_TIME);
+	}
+	
+	@SuppressWarnings("unused")
+	@Test (expected = PizzaException.class)
+	public void testPizzaFactoryDeliveryTooEarly() throws PizzaException {
+		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, ORDER_TIME, ORDER_TIME.plusMinutes(1));
+	}
+	
+	@SuppressWarnings("unused")
+	@Test
+	public void testPizzaFactoryDeliveryShortlyAfterClose() throws PizzaException {
+		LocalTime orderTime = LocalTime.of(11, 00);
+		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, orderTime, orderTime.plusMinutes(15));
+	}
+	
+	@SuppressWarnings("unused")
+	@Test 
+	public void testPizzaFactoryDeliveryExactlyOneHourAfterOrder() throws PizzaException {
+		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, ORDER_TIME, ORDER_TIME.plusHours(1));
+	}
+	
+	@SuppressWarnings("unused")
+	@Test (expected = PizzaException.class)
+	public void testPizzaFactoryDeliveryOverOneHourAfterOrder() throws PizzaException {
+		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, ORDER_TIME, ORDER_TIME.plusHours(2));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/asgn2Tests/PizzaTests.java
+++ src/asgn2Tests/PizzaTests.java
@@ -1,5 +1,20 @@
 package asgn2Tests;
 
+import static org.junit.Assert.*;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import asgn2Exceptions.PizzaException;
+import asgn2Pizzas.MargheritaPizza;
+import asgn2Pizzas.MeatLoversPizza;
+import asgn2Pizzas.PizzaTopping;
+import asgn2Pizzas.VegetarianPizza;
+
 /**
  * A class that that tests the asgn2Pizzas.MargheritaPizza, asgn2Pizzas.VegetarianPizza, asgn2Pizzas.MeatLoversPizza classes. 
  * Note that an instance of asgn2Pizzas.MeatLoversPizza should be used to test the functionality of the 
@@ -9,5 +24,98 @@ package asgn2Tests;
  *
  */
 public class PizzaTests {
-	// TO DO
+	private static final int QUANTITY = 3;
+	private static final LocalTime ORDER_TIME = LocalTime.of(20, 0);
+	private static final LocalTime DELIVERY_TIME = LocalTime.of(20, 15);
+	private static final double MEATLOVERS_PRICE = 12.0;
+	private MeatLoversPizza meatlovers;
+	private VegetarianPizza vegetarian;
+	private MargheritaPizza margherita;
+	private double meatLoversCost;
+	private final ArrayList<PizzaTopping> meatLoversToppings = new ArrayList<PizzaTopping>(Arrays.asList(
+		PizzaTopping.TOMATO,
+		PizzaTopping.CHEESE,
+		PizzaTopping.BACON,
+		PizzaTopping.PEPPERONI,
+		PizzaTopping.SALAMI
+	));
+	
+	// tests that all extended pizza classes initialise properly
+	@Before
+	@Test
+	public void testPizzaInit() throws PizzaException {
+		meatlovers = new MeatLoversPizza(QUANTITY, ORDER_TIME, DELIVERY_TIME);
+		vegetarian = new VegetarianPizza(QUANTITY, ORDER_TIME, DELIVERY_TIME);
+		margherita = new MargheritaPizza(QUANTITY, ORDER_TIME, DELIVERY_TIME);
+		
+		for(PizzaTopping topping : meatLoversToppings) {
+			meatLoversCost += topping.getCost();
+		}
+	}
+	
+	// ---------------------------- Pizza Tests ------------------------------//
+	
+	// tests calculateCostPerPizza() and getCostPerPizza() normally
+	@Test
+	public void testCalculateCostPerPizza() {
+		meatlovers.calculateCostPerPizza();
+		assertEquals(meatLoversCost, meatlovers.getCostPerPizza(), 0);
+	}
+	
+	// tests getCostPerPizza() for when the cost hasn't been calculated yet
+	@Test
+	public void testGetCostPerPizzaNotCalculated() {
+		assertEquals(0.0, meatlovers.getCostPerPizza(), 0);
+	}
+	
+	@Test
+	public void testGetPricePerPizza() {
+		assertEquals(MEATLOVERS_PRICE, meatlovers.getPricePerPizza(), 0);
+	}
+	
+	@Test
+	public void testGetOrderCost() {
+		meatlovers.calculateCostPerPizza();
+		assertEquals(meatLoversCost * QUANTITY, meatlovers.getOrderCost(), 0);
+	}
+	
+	// test getOrderCost() for when the cost hasn't been calculated yet
+	@Test
+	public void testGetOrderCostNotCalculated() {
+		assertEquals(0.0, meatlovers.getOrderCost(), 0);
+	}
+	
+	@Test
+	public void testGetOrderPrice() {
+		assertEquals(MEATLOVERS_PRICE * QUANTITY, meatlovers.getOrderPrice(), 0);
+	}
+	
+	@Test
+	public void testGetOrderProfit() {
+		meatlovers.calculateCostPerPizza();
+		assertEquals(MEATLOVERS_PRICE * QUANTITY - meatLoversCost * QUANTITY, 
+				meatlovers.getOrderProfit(), 0);
+	}
+	
+	// tests containsTopping() with a topping that is on meatlovers pizza
+	@Test
+	public void testContainsToppingToppingExists() {
+		assertTrue(meatlovers.containsTopping(meatLoversToppings.get(0)));
+	}
+	
+	// tests containsTopping() with a topping that isn't on meatlovers pizza
+	@Test
+	public void testContainsToppingToppingDoesntExist() {
+		assertFalse(meatlovers.containsTopping(PizzaTopping.EGGPLANT));
+	}
+	
+	@Test
+	public void testGetQuantity() {
+		assertEquals(QUANTITY, meatlovers.getQuantity());
+	}
+	
+	@Test
+	public void testGetPizzaType() {
+		assertEquals("Meat Lovers", meatlovers.getPizzaType());
+	}
 }
