d6948832c7543e89be83abc5a41b5f14a4397b88
Daniel Gilchrist <danielgilchrist@outlook.com.au>
2017-06-01 17:33:41 +1000

    Implemented methods in LogHandler.java

--- src/asgn2Restaurant/LogHandler.java
+++ src/asgn2Restaurant/LogHandler.java
@@ -1,13 +1,19 @@
 package asgn2Restaurant;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 import asgn2Customers.Customer;
+import asgn2Customers.CustomerFactory;
 import asgn2Exceptions.CustomerException;
 import asgn2Exceptions.LogHandlerException;
 import asgn2Exceptions.PizzaException;
 import asgn2Pizzas.Pizza;
+import asgn2Pizzas.PizzaFactory;
 
 /**
  *
@@ -38,9 +44,27 @@ public class LogHandler {
      *             semantic errors above
      * 
      */
-    public static ArrayList<Customer> populateCustomerDataset(String filename)
-            throws CustomerException, LogHandlerException {
-        // TO DO
+    public static ArrayList<Customer> populateCustomerDataset(String filename) 
+    		throws CustomerException, LogHandlerException {
+    	
+    	ArrayList<Customer> customers = new ArrayList<>();
+        
+        try {
+        	BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+            String line = null;
+            
+            while ((line = bufferedReader.readLine()) != null) {
+            	customers.add(createCustomer(line));
+            }
+            
+            bufferedReader.close();
+        } catch (IOException e) {
+        	throw new LogHandlerException(e.toString());
+        } catch (CustomerException e) {
+        	throw new CustomerException(e.toString());
+        }
+        
+        return customers;
     }
 
     /**
@@ -61,8 +85,27 @@ public class LogHandler {
      *             semantic errors above
      * 
      */
-    public static ArrayList<Pizza> populatePizzaDataset(String filename) throws PizzaException, LogHandlerException {
-        // TO DO
+    public static ArrayList<Pizza> populatePizzaDataset(String filename) 
+    		throws PizzaException, LogHandlerException {
+    	
+    	ArrayList<Pizza> pizzas = new ArrayList<>();
+        
+        try {
+        	BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+            String line = null;
+            
+            while ((line = bufferedReader.readLine()) != null) {
+            	pizzas.add(createPizza(line));
+            }
+            
+    		bufferedReader.close();
+        } catch (IOException e) {
+        	throw new LogHandlerException(e.toString());
+        } catch (PizzaException e) {
+        	throw new PizzaException(e.toString());
+        }
+        
+        return pizzas;
     }
 
     /**
@@ -83,7 +126,9 @@ public class LogHandler {
      *             - If there was a problem parsing the line from the log file.
      */
     public static Customer createCustomer(String line) throws CustomerException, LogHandlerException {
-        // TO DO
+        String[] words = line.split(",");
+        return CustomerFactory.getCustomer(words[4], words[2], words[3], 
+        		Integer.parseInt(words[5]), Integer.parseInt(words[6]));
     }
 
     /**
@@ -104,7 +149,9 @@ public class LogHandler {
      *             - If there was a problem parsing the line from the log file.
      */
     public static Pizza createPizza(String line) throws PizzaException, LogHandlerException {
-        // TO DO
+        String[] words = line.split(",");
+        return PizzaFactory.getPizza(words[7], Integer.parseInt(words[8]), 
+				LocalTime.parse(words[0]), LocalTime.parse(words[1]));
     }
 
 }
