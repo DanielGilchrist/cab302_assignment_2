029d2082f536e4eaa938bd909e42da783b4c5323
Daniel Gilchrist <danielgilchrist@outlook.com.au>
2017-06-01 19:29:54 +1000

    Changed the SuppressWarning("unused") annotations to just one annotation above the class

--- src/asgn2Tests/PizzaFactoryTests.java
+++ src/asgn2Tests/PizzaFactoryTests.java
@@ -18,6 +18,7 @@ import asgn2Pizzas.VegetarianPizza;
  * @author Person B 
  * 
  */
+@SuppressWarnings("unused")
 public class PizzaFactoryTests {
 	private static final int QUANTITY = 3;
 	private static final LocalTime ORDER_TIME = LocalTime.of(20, 0);
@@ -28,7 +29,6 @@ public class PizzaFactoryTests {
 	private static String[] pizzaCodes = {"PZL", "PZV", "PZM"};
 	
 	// tests PizzaFactory for all pizzas with valid parameters
-	@SuppressWarnings("unused")
 	@Test
 	public void testPizzaFactoryLegalValues() throws PizzaException {
 		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, ORDER_TIME, DELIVERY_TIME);
@@ -36,64 +36,54 @@ public class PizzaFactoryTests {
 		Pizza margherita = PizzaFactory.getPizza(pizzaCodes[2], QUANTITY, ORDER_TIME, DELIVERY_TIME);
 	}
 	
-	@SuppressWarnings("unused")
 	@Test (expected = PizzaException.class)
 	public void testPizzaFactoryInvalidPizzaCode() throws PizzaException {
 		Pizza meatlovers = PizzaFactory.getPizza("", QUANTITY, ORDER_TIME, DELIVERY_TIME);
 	}
 	
-	@SuppressWarnings("unused")
 	@Test (expected = PizzaException.class)
 	public void testPizzaFactoryQuantityTooLow() throws PizzaException {
 		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], 0, ORDER_TIME, DELIVERY_TIME);
 	}
 	
-	@SuppressWarnings("unused")
 	@Test (expected = PizzaException.class)
 	public void testPizzaFactoryQuantityTooHigh() throws PizzaException {
 		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], 11, ORDER_TIME, DELIVERY_TIME);
 	}
 	
-	@SuppressWarnings("unused")
 	@Test (expected = PizzaException.class)
 	public void testPizzaFactoryOrderTooEarly() throws PizzaException {
 		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, LocalTime.of(18, 0), DELIVERY_TIME);
 	}
 	
-	@SuppressWarnings("unused")
 	@Test
 	public void testPizzaFactoryOrderOnOpeningTime() throws PizzaException {
 		LocalTime orderTime = LocalTime.of(19, 0);
 		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, orderTime, orderTime.plusMinutes(20));
 	}
 	
-	@SuppressWarnings("unused")
 	@Test
 	public void testPizzaFactoryOrderOnCloseTime() throws PizzaException {
 		LocalTime orderTime = LocalTime.of(23, 0);
 		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, orderTime, orderTime.plusMinutes(20));
 	}
 	
-	@SuppressWarnings("unused")
 	@Test (expected = PizzaException.class)
 	public void testPizzaFactoryDeliveryTooEarly() throws PizzaException {
 		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, ORDER_TIME, ORDER_TIME.plusMinutes(1));
 	}
 	
-	@SuppressWarnings("unused")
 	@Test
 	public void testPizzaFactoryDeliveryShortlyAfterClose() throws PizzaException {
 		LocalTime orderTime = LocalTime.of(23, 00);
 		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, orderTime, orderTime.plusMinutes(15));
 	}
 	
-	@SuppressWarnings("unused")
 	@Test 
 	public void testPizzaFactoryDeliveryExactlyOneHourAfterOrder() throws PizzaException {
 		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, ORDER_TIME, ORDER_TIME.plusHours(1));
 	}
 	
-	@SuppressWarnings("unused")
 	@Test (expected = PizzaException.class)
 	public void testPizzaFactoryDeliveryOverOneHourAfterOrder() throws PizzaException {
 		Pizza meatlovers = PizzaFactory.getPizza(pizzaCodes[0], QUANTITY, ORDER_TIME, ORDER_TIME.plusHours(2));
